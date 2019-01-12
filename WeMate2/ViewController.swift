e41ae6e6b51f45c8f02ac9468d28246a59903ede
Yash Nayak <yugn27@yashn-2.local>
2019-01-12 22:12:09 +0530

    Initial Commit

---
+++ WeMate2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeMate2
+//
+//  Created by Yash Nayak on 12/01/19.
+//  Copyright © 2019 Yash Nayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
